6b74566acf33aa7a84bf1ccb584f71a721aba1fa
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-11-07 07:06:34 +0900

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
+namespace poitch3 {
 
+    let initFlag = 0;
+
+    function initSwitch(): void {
+        pins.setPull(DigitalPin.P8, PinPullMode.PullDown);
+        pins.setPull(DigitalPin.P12, PinPullMode.PullDown);
+        pins.setPull(DigitalPin.P13, PinPullMode.PullDown);
+        pins.setPull(DigitalPin.P16, PinPullMode.PullDown);
+        initFlag=1;
+    }
+    /**
+     * get switch data
+     * @param sw test pin of switch
+     */
+    //% blockId=testSwitch block="test switch at|%sw"
+    export function testSwitch(sw : DigitalPin): boolean {
+		if (initFlag == 0){
+			initSwitch();
+		}
+        if (pins.digitalReadPin(sw)==1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
